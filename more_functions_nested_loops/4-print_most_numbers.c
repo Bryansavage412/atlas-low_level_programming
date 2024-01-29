3d2b1b8b80579d7759fb638cd7910df6b420f6d1
Bryansavage412 <109620803+Bryansavage412@users.noreply.github.com>
2024-01-29 18:30:28 +0000

    betty

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,51 +8,20 @@
 
 void print_most_number(void)
 
-{ 	
+{
 
 	int x = 0;
 
 	for (; x <= 9; x++)
 	{
-	if (x == 2 || x == 4)
-	{
-	continue;
-	}
-	else
-	{
-	_putchar(x + '0');
-	}
-	}
-	_putchar('\n');
+		if (x == 2 || x == 4)
+		{
+		continue;
+		}
+		else
+		{
+		_putchar(x + '0');
+		}
+		}
+		_putchar('\n');
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
