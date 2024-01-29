bcfaaba2349f17bc141ef33f18415cfd4e6ab6e9
Bryansavage412 <109620803+Bryansavage412@users.noreply.github.com>
2024-01-29 18:41:29 +0000

    gcc

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  * main -check the code
+ *  * main - check the code
  *   *
  *    * Return: Always 0.
  *     */
-int main(void)
+void print_line(int n)
 {	
 	print_line(0);
 	 print_line(2);
