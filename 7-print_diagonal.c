e293ded42efb266cf10e8092689aa5d1447b1586
Bryansavage412 <109620803+Bryansavage412@users.noreply.github.com>
2024-01-30 01:30:07 +0000

    figuring out how to use loops

---
+++ 7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**print_diagonal - Draws a diagonal line according parameter
+@n; The number of the times to print diagomal lines
+*/
+
+void print_diagonal(int n)
+
+{
+    int x y;
+
+    if (n <=0)
+    {
+        _putchar('\n');
+    }
+    else
+    {
+        for (x = 0; x < n; x++)
+    {
+        for (y = 0; y < x; y++)
+    {
+        _putchar(64);
+    }
+    _putchar(184);
+    _putchar('\n');
+    }
+    }
+}
