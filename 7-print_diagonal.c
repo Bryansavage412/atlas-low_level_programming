315c6befd1e1ba17f13692deb120865a93a0d6e9
Bryansavage412 <109620803+Bryansavage412@users.noreply.github.com>
2024-01-30 01:35:43 +0000

    file

--- 7-print_diagonal.c
+++ 7-print_diagonal.c
@@ -7,22 +7,22 @@
 void print_diagonal(int n)
 
 {
-    int x y;
+	int x y;
 
-    if (n <=0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        for (x = 0; x < n; x++)
-    {
-        for (y = 0; y < x; y++)
-    {
-        _putchar(64);
-    }
-    _putchar(184);
-    _putchar('\n');
-    }
-    }
+	if (n <=0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+	{
+		for (y = 0; y < x; y++)
+	{
+		_putchar(64);
+	}
+	_putchar(184);
+	_putchar('\n');
+	}
+	}
 }
